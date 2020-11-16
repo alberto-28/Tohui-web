4f441a73a77ff023a210115c93a03dee8c70dbc4
alberto-28 <luisalbertogardunobernabe@gmail.com>
2020-11-16 00:47:03 -0600

    Update Beto

--- js/Mijs.js
+++ js/Mijs.js
@@ -1,3 +1,17 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: './animations/anim.json'
+})
+
+LottieContainer.addEventListener('click', function() {
+  lottieAnimation.play();
+});
+
+
 $('.carousel').carousel({
     interval: 3000
   })
+
